94112c8dfc4d633b7435e4d377276091ae35a2a6
meryann26 <meryannc26@gmail.com>
2023-10-25 16:01:31 -0600

    Muestra de nombres en Bitácora y Reservas
    En lugar de mostrar id's ahora muestra los nombres según corresponda

--- src/java/com/appcondominio/service/ReservaAmenidadTO.java
+++ src/java/com/appcondominio/service/ReservaAmenidadTO.java
@@ -12,22 +12,34 @@ public class ReservaAmenidadTO implements Serializable{
     
     private Integer idReservaAmenidad;
     private Integer idAmenidad; 
+    private String nombreAmenidad;
+    private String descripcionAmenidad;
     private Timestamp fechaInicio;
     private Timestamp fechaFin;
     private Integer cedulaResidente;
+    private String nombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private int numeroCasa;
     private String estado;
+    private ResidenteTO residente;
+    private AmenidadTO amenidad;
 
     public ReservaAmenidadTO(){
         
     }
-    public ReservaAmenidadTO(Integer idReservaAmenidad, Integer idAmenidad, Timestamp fechaInicio, Timestamp fechaFin, Integer cedulaResidente, String estado) {
-        this.idReservaAmenidad = idReservaAmenidad;
-        this.idAmenidad = idAmenidad;
-        this.fechaInicio = fechaInicio;
-        this.fechaFin = fechaFin;
-        this.cedulaResidente = cedulaResidente;
-        this.estado = estado;
-    }
+    public ReservaAmenidadTO(Integer idReservaAmenidad, String nombreAmenidad, String descripcionAmenidad, Timestamp fechaInicio, Timestamp fechaFin, String nombre, String primerApellido, String segundoApellido, int numeroCasa, String estado) {
+    this.idReservaAmenidad = idReservaAmenidad;
+    this.nombreAmenidad = nombreAmenidad;
+    this.descripcionAmenidad = descripcionAmenidad;
+    this.fechaInicio = fechaInicio;
+    this.fechaFin = fechaFin;
+    this.nombre = nombre;
+    this.primerApellido = primerApellido;
+    this.segundoApellido = segundoApellido;
+    this.numeroCasa = numeroCasa;
+    this.estado = estado;
+}
 
     public Integer getIdReservaAmenidad() {
         return idReservaAmenidad;
@@ -77,7 +89,80 @@ public class ReservaAmenidadTO implements Serializable{
         this.estado = estado;
     }
 
+    public String getNombreAmenidad() {
+        return nombreAmenidad;
+    }
+
+    public void setNombreAmenidad(String nombreAmenidad) {
+        this.nombreAmenidad = nombreAmenidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public int getNumeroCasa() {
+        return numeroCasa;
+    }
+
+    public void setNumeroCasa(int numeroCasa) {
+        this.numeroCasa = numeroCasa;
+    }
+
+    public String getDescripcionAmenidad() {
+        return descripcionAmenidad;
+    }
+
+    public void setDescripcionAmenidad(String descripcionAmenidad) {
+        this.descripcionAmenidad = descripcionAmenidad;
+    }
     
     
 
+  
+    
+    
+    
+    public ResidenteTO getResidente() {
+        return residente;
+    }
+
+    public void setResidente(ResidenteTO residente) {
+        this.residente = residente;
+    }
+
+    public AmenidadTO getAmenidad() {
+        return amenidad;
+    }
+
+    public void setAmenidad(AmenidadTO amenidad) {
+        this.amenidad = amenidad;
+    }
 }
+    
+    
+
+    
+    
+
+
